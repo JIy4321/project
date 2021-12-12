efbe84addb753d39c7f7f6672055ab1f5bce1c2c
JIy4321 <90255079+JIy4321@users.noreply.github.com>
2021-12-12 21:15:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var x=document.getElementById("myAudio4")
+function playSorry()
+{
+x.play();
+}
+var a=document.getElementById("myAudio1")
+function playCompany(){
+    a.play();
+}
+
+var b=document.getElementById("myAudio2")
+function playPeaches(){
+    b.play();
+    a.stop();
+}
+
+var j=document.getElementById("myAudio")
+function playLetMeLoveYou(){
+    j.play();
+}
+
+var i=document.getElementById("myAudio6")
+function playHoldOn(){
+    i.play();
+}
+var w=document.getElementById("myAudio5")
+function playWhatDoYouMean()
+{
+    w.play();
+}
+var s=document.getElementById("myAudio3")
+function playStay()
+{
+    s.play();
+}
+var n=document.getElementById("myAudio7")
+function playnsn()
+{
+    n.play();
+}
